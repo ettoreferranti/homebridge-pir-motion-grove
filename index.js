a3d12ba9de7711ecd8f091fd320e1dce2745a35c
Ettore Ferranti <ettore@ettores-macbook-pro.home>
2020-11-04 15:29:51 +0100

    test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class MotionSensor {
           console.log('Something wrong just happened');
           console.log(err);
         },
-        onInit: MotionSensor.initMethod
+        onInit: initMethod
     });
       
     board.init();
